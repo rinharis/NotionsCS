0a9831d3b6b852a3f2cbc208935e381c2ce0ac0d
rin HARISON <rinharis@gmail.com>
2020-11-06 10:40:00 +0100

    Ajoutez des fichiers projet.

---
+++ Generique/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

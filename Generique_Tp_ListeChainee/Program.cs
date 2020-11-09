ab8a88ee3c42bbf4f17d99f7522ff70f36552d68
rin HARISON <rinharis@gmail.com>
2020-11-09 16:05:13 +0100

    Fin classe generique

---
+++ Generique_Tp_ListeChainee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generique_Tp_ListeChainee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

9e1c45e9a4f652871b56a332aac0eef2ae2510e2
rin HARISON <rinharis@gmail.com>
2020-11-18 20:10:07 +0100

    Exception management demo

---
+++ Exception/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestException exception = new TestException();
+        }
+    }
+}

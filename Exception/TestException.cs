fd9735815317367260659cbe69630513840fb39d
rin HARISON <rinharis@gmail.com>
2020-11-18 20:10:07 +0100

    Exception management demo

---
+++ Exception/TestException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exception
+{
+    internal class TestException
+    {
+        public TestException()
+        {
+            Demo_Exception();
+        }
+
+        private void Demo_Exception()
+        {
+            try
+            {
+                string chaine = "dix";
+                int valeur = Convert.ToInt32(chaine);
+                Console.WriteLine("Ce code ne sera jamais affiché");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Une erreur s'est produite dans la tentative de conversion");
+            }
+        }
+    }
+}
